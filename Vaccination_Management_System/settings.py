f03ac4481cb0ba22b92ff886ea6e8e6f750d603e
mislamdev <mislamdev@mitech.stream>
2025-09-28 22:33:15 +0600

    Add environment variable support and update requirements
    - Updated settings.py to load environment variables for SECRET_KEY, DEBUG, and ALLOWED_HOSTS.
    - Added static and media file configurations in settings.py.
    - Updated requirements.txt to include dj-database-url, psycopg2-binary, python-dotenv, and whitenoise.
    - Modified .gitignore to include .env and static/ directories.
    - Updated urls.py to serve static and media files during development.

--- Vaccination_Management_System/settings.py
+++ Vaccination_Management_System/settings.py
@@ -9,22 +9,26 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
-
+import os
 from pathlib import Path
+from dotenv import load_dotenv
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+load_dotenv(BASE_DIR / '.env')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-+kbbcgjp86nlsk$=ngw5k38&pyds5f4z!u-!s3cl)v+x9klgy8"
+SECRET_KEY = os.environ.get("SECRET_KEY", "django-insecure-+kbbcgjp86nlsk$=ngw5k38&pyds5f4z!u-!s3cl)v+x9klgy8")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DEBUG", "False") == "True"
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "*").split(",")
 
 # Application definition
 
@@ -39,10 +43,12 @@ INSTALLED_APPS = [
     "users",
     "api",
     "drf_spectacular",
+    "whitenoise.runserver_nostatic",
 ]
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
@@ -76,10 +82,7 @@ WSGI_APPLICATION = "Vaccination_Management_System.wsgi.application"
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+    "default": dj_database_url.parse(os.environ.get("POSTGRES_URL_NON_POOLING") or os.environ.get("POSTGRES_URL") or "")
 }
 
 # Password validation
@@ -127,6 +130,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = "static/"
+MEDIA_URL = "media/"
+STATICFILES_DIRS = [BASE_DIR / "staticfiles"]
+STATIC_ROOT = BASE_DIR / "static"
+MEDIA_ROOT = BASE_DIR / "media"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
